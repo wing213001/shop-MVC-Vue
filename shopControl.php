9520a723b612243bc21430b33b25ceec650f82a9
DESKTOP-Q2AKQ2P\tiger <s110213002@mail1.ncnu.edu.tw>
2023-11-17 16:43:24 +0800

    control clean up and edit

--- shopControl.php
+++ shopControl.php
@@ -1,38 +1,43 @@
 <?php
 require('shopModel.php');
 
+
 $act=$_REQUEST['act'];
 switch ($act) {
 case "listJob":
   $jobs=getJobList();
   echo json_encode($jobs);
   return;  
+
 case "listJob2":
   $jobs=getJobList2();
   echo json_encode($jobs);
   return;
-  
+
+
+
 case "addJob":
-    //擷取了 POST 資料中名為 "dat" 的資料轉成JSON字串
-	$jsonStr = $_POST['dat']; 
-	//將 JSON 字串轉換成對應 PHP 物件的函式
-	$job = json_decode($jsonStr); 
-	//should verify first 
+	$jsonStr = $_POST['dat'];
+	$job = json_decode($jsonStr);
+	//should verify first
 	addJob($job->jobName,$job->jobContent,$job->price,$job->buyNum,$job->id,$job->totalQuantity);
-	return; 
-	
+	return;
+
 case "addJob2":
-	
 	$jsonStr = $_POST['dat'];
 	$job = json_decode($jsonStr);
 	//should verify first
 	addJob2($job->buyNum,$job->id);
 	return;
+
+
+
 case "delJob":
 	$id=(int)$_REQUEST['id']; //$_GET, $_REQUEST
 	//verify
 	delJob($id);
 	return;
+
 case "delJob2":
 	$id=(int)$_REQUEST['id']; //$_GET, $_REQUEST
 	//verify
@@ -41,5 +46,6 @@ case "delJob2":
 default:
   
 }
+
 
 ?>
